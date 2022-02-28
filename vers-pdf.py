284d44e944d9b512a077fe26bd73957a1fdd1dc8
Olivier <olivierdabl@oliviers-macbook-pro.home>
2022-02-28 12:08:39 +0100

    Rectification vers pdf originel

--- vers-pdf.py
+++ vers-pdf.py
@@ -1,24 +1,20 @@
-import os
-variables = ""
-variables += "\\def\\nommois{février} "
-variables += "\\def\\numeromois{02} "
-variables += "\\def\\annee{2022} "
-variables += "\\def\\jourdepaiement{10} "
-variables += "\\def\\loyerchiffres{350} "
-variables += "\\def\\loyerlettres{trois cent cinquante} "
-variables += "\\def\\charges{0} "
-variables += "\\def\\economisenergies{0} "
-variables += "\\def\\nomproprio{Mr Tintin Journaliste} "
-variables += "\\def\\adresseproprio{Ici, Batiment La Bas} "
-variables += "\\def\\villeproprio{81367 La Fourche} "
-variables += "\\def\\nomlocataire{Mr Tony Haddock} "
-variables += "\\def\\adresselocataire{16 rue des Marins d'Eau Douce} "
-variables += "\\def\\villelocataire{12345 Sabords} "
-variables += "\\def\\adresselocation{16 rue des Duponts, Appartement 4} "
-variables += "\\def\\villelocation{54321 Jumeaux} "
-variables += "\\def\\lieudocument{Paris} "
-variables += "\\def\\debutmois{01/02/2022} "
-variables += "\\def\\finmois{28/02/2022} "
-variables += "\\def\\totalpaiement{350} "
-variables += "\\def\\datepaiement{10/02/2022} "
-os.system(" pdflatex \""+variables+" \input{modele-quittance-appt-meuble-vide.tex}\" ")
+# import os
+# variables = ""
+# variables += "\\def\\jourdepaiement{10} "
+# variables += "\\def\\loyerchiffres{350} "
+# variables += "\\def\\loyerlettres{trois cent cinquante} "
+# variables += "\\def\\charges{0} "
+# variables += "\\def\\economisenergies{0} "
+
+# variables += "\\def\\nomlocataire{Mr Tony Haddock} "
+# variables += "\\def\\adresselocataire{16 rue des Marins d'Eau Douce} "
+# variables += "\\def\\villelocataire{12345 Sabords} "
+# variables += "\\def\\adresselocation{16 rue des Duponts, Appartement 4} "
+# variables += "\\def\\villelocation{54321 Jumeaux} "
+# variables += "\\def\\lieudocument{Paris} "
+# variables += "\\def\\totalpaiement{350} "
+# variables += "\\def\\datepaiement{10/02/2022} "
+# os.system(" pdflatex \""+variables+" \input{modele-quittance-appt-meuble-vide.tex}\" ")
+
+import GenerateurQuittances
+generateur = GenerateurQuittances("bdd.yml")
