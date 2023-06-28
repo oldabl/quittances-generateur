9f9ef7a4fe42e95d9fb1d7f7dd37027e425ce96d
Olivier Brunel <obrunel@proton.me>
2023-06-28 20:00:57 +0200

    Gestion des impayes

--- generateur-quittances.py
+++ generateur-quittances.py
@@ -18,10 +18,11 @@ mois_francais = { "01": "janvier",
 # Classe qui permet de générer des quittances en PDF à partir d'un fichier YAML
 #
 class GenerateurQuittances:
-  def __init__(self, fichier_bdd, mois = datetime.datetime.now().strftime("%m"), annee = datetime.datetime.now().strftime("%Y")):
+  def __init__(self, fichier_bdd, mois = datetime.datetime.now().strftime("%m"), annee = datetime.datetime.now().strftime("%Y"), premierjour = 1):
     self.fichier_bdd = fichier_bdd
     self.mois = str(mois).zfill(2)
     self.annee = str(annee)
+    self.premierjour = str(premierjour)
   def creerQuittances(self):
     self.bddVersQuittances()
 
@@ -37,14 +38,18 @@ class GenerateurQuittances:
   # Crée une chaine de variables avec les informations du locataire du lot
   def rapporterVariablesInfoLocatairePourLatex(self, lot):
     variables = ""
-    variables += " \\def\\loyerchiffres{"+str(lot['montant_loyer'])+"} "
-    variables += " \\def\\charges{"+str(lot['montant_charges'])+"} "
+    variables += " \\def\\lotlocation{"+str(lot['numero'])+"} "
     variables += " \\def\\nomlocataire{"+str(lot['nom_locataire'])+"} "
     variables += " \\def\\adresselocataire{"+str(lot['adresse_locataire'])+"} "
     variables += " \\def\\villelocataire{"+str(lot['ville_locataire'])+"} "
-    variables += " \\def\\lotlocation{"+str(lot['numero'])+"} "
-    variables += " \\def\\totalpaiementchiffres{"+str(lot['montant_total_chiffres'])+"} "
-    variables += " \\def\\totalpaiementlettres{"+str(lot['montant_total_lettres'])+"} "
+    variables += " \\def\\typedelot{"+str(lot['type_lot'])+"} "
+    variables += " \\def\\loyerchiffres{"+str(lot['montant_loyer'])+"} "
+    variables += " \\def\\charges{"+str(lot['montant_charges'])+"} "
+    variables += " \\def\\loyerccchiffres{"+str(lot['montant_total_chiffres'])+"} "
+    variables += " \\def\\loyercclettres{"+str(lot['montant_total_lettres'])+"} "
+    variables += " \\def\\montantrecuchiffres{"+str(lot['montant_recu_chiffres'])+"} "
+    variables += " \\def\\montantreculettres{"+str(lot['montant_recu_lettres'])+"} "
+    variables += " \\def\\montantimpayechiffres{"+str(round(float(lot['montant_total_chiffres'])-float(lot['montant_recu_chiffres']), 2))+"} "
     variables += " \\def\\datepaiement{"+str(lot['jour_paiement_mois']).zfill(2)+"/"+self.mois+"/"+self.annee+"} "
     return variables
 
@@ -58,7 +63,7 @@ class GenerateurQuittances:
   # Crée une chaine de variables avec les informations sur les différentes dates nécessaires
   def creerVariablesDatePourLatex(self):
     variables = ""
-    date = datetime.datetime.strptime(self.annee+"-"+self.mois+"-01", "%Y-%m-%d")
+    date = datetime.datetime.strptime(self.annee+"-"+self.mois+"-"+str(self.premierjour).zfill(2), "%Y-%m-%d")
     jour_fin_mois = str(calendar.monthrange(int(self.annee), int(self.mois))[1])
     variables += " \\def\\nommois{"+str(mois_francais[date.strftime("%m")])+"} "
     variables += " \\def\\numeromois{"+str(date.strftime("%m"))+"} "
@@ -84,9 +89,14 @@ class GenerateurQuittances:
           toutes_variables_necessaires += self.rapporterVariablesInfoLocatairePourLatex(lot)
           toutes_variables_necessaires = " ".join(toutes_variables_necessaires.split())
           nom_famille_locataire = self.rapporterNomLocataire(lot)
-          nom_fichier = "Quittance loyer "+nom_famille_locataire+" "+self.annee+"."+self.mois
-          os.system(" pdflatex -jobname '"+nom_fichier+"' -output-format pdf \""+toutes_variables_necessaires+" \input{modele-quittance-"+lot['type_lot']+".tex}\" > "+nom_fichier+".log")
-          print("Quittance pour "+lot['nom_locataire']+" générée dans '"+nom_fichier+".pdf'")
+          recuouquittance = "quittance"
+          recuouquittancetexte = "Quittance"
+          if lot['montant_total_chiffres'] != lot['montant_recu_chiffres'] or lot['montant_total_lettres'] != lot['montant_recu_lettres']:
+            recuouquittance = "reculoyer"
+            recuouquittancetexte = "Recu"
+          nom_fichier = recuouquittancetexte+" loyer "+nom_famille_locataire+" "+self.annee+"."+self.mois
+          os.system(" pdflatex -jobname '"+nom_fichier+"' -output-format pdf \""+toutes_variables_necessaires+" \input{modele-"+recuouquittance+"-"+lot['type_lot']+".tex}\" > "+nom_fichier+".log")
+          print(recuouquittancetexte+" de loyer pour "+lot['nom_locataire']+" générée dans '"+nom_fichier+".pdf'")
           if 'dossier_disque' in bdd['proprietaire']:
             destination = bdd['proprietaire']['dossier_disque']+"/Bien - Immeuble "+adresse_info['adresse_courte']+"/Location/"+lot['numero']+"/Locataire courant - "+lot['nom_locataire']
             text = input("Copier dans '"+destination+"' ? (y/N)'")
